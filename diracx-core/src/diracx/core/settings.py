34424e2e508ee26c72961a7dcb78f63b07cbf1ab
Stella-Maria Renucci <stellarenucci04@gmail.com>
2026-02-10 18:05:24 +0100

    fix: removed EdDSA from test and documentation

--- diracx-core/src/diracx/core/settings.py
+++ diracx-core/src/diracx/core/settings.py
@@ -203,13 +203,13 @@ class AuthSettings(ServiceSettingsBase):
     This includes both public and private keys for token signature
     generation and verification.
     """
-    
+
     # TODO: EdDSA should be removed later due to "SecurityWarning: EdDSA is deprecated via RFC 9864"
     token_allowed_algorithms: list[str] = ["RS256", "EdDSA", "Ed25519"]  # noqa: S105
     """List of allowed cryptographic algorithms for JWT token signing.
 
-    Supported algorithms include RS256 (RSA with SHA-256), EdDSA and Ed25519
-    (Edwards-curve Digital Signature Algorithm). Default: ["RS256", "EdDSA", "Ed25519"]
+    Supported algorithms include RS256 (RSA with SHA-256) and Ed25519
+    (Edwards-curve Digital Signature Algorithm). Default: ["RS256", "Ed25519"]
     """
 
     access_token_expire_minutes: int = 20
